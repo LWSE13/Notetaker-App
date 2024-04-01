7f5b68fea97b1cc9ea127d4ef29034e31c5154c3
LWSE13 <leewheeler888@gmail.com>
2024-04-01 20:19:03 +0100

    added post and delete methods to the notes route

--- routes/notes.js
+++ routes/notes.js
@@ -30,3 +30,49 @@ notesROUTE.get('/:note_id', async (req, res) => {
   }
 });
 
+// POST Route 
+notesROUTE.post('/', async (req, res) => {
+  console.info(`notes ${req.method} request received`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    try {
+      await readAndAppend(newNote, './db/db.json');
+      res.json('Note added');
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Error in adding note');
+    }
+  } else {
+    res.status(400).json('Error: title and text are required');
+  }
+});
+
+// DELETE Route
+notesROUTE.delete('/:note_id', async (req, res) => {
+  const note_id = req.params.note_id;
+  console.info(`notes ${req.method} for id ${note_id} request received`);
+
+  try {
+    const data = await readFromFile('./db/db.json');
+    const json = JSON.parse(data);
+    // filter out the new note
+    const result = json.filter((note) => note.note_id !== note_id);
+
+    writeToFile('./db/db.json', result);
+
+    res.json('Note deleted');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Error in deleting note');
+  }
+});
+
+module.exports = notesROUTE;
